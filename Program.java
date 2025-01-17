0ebfbeeb6eb987107e2b0c69ceae50eb8d5c2970
Aleksandry Cesar <57228025+KoiGunner@users.noreply.github.com>
2025-01-17 17:47:23 -0300

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,22 @@
+import funcoes.Calculadora;
+import funcoes.MathFunctions;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		//Testes efetuados com sucesso nas operações básicas
+		//Calculadora.divide(4, 2);
+		//Calculadora.multiply(4, 2);
+		//Calculadora.sum(4, 2);
+		//Calculadora.reduce(4, 2);
+		//System.out.println("   ");
+		//System.out.println(" - ");
+		
+		//Tabuada com números variados 
+		MathFunctions.multiTable(5);
+		MathFunctions.potentiation(2, 3);
+		MathFunctions.squareRoot(25);
+		
+	}
+
+}
